4918742ae30b46ce5a01fce4a1e301dc1d80d6dd
Nedra Mevoli ♥ <nedra.mevoli@willowtreeapps.com>
2022-07-26 09:31:33 -0400

    Layout still needs work, but UI and most functionality is up

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -15,34 +15,55 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        ZStack {
-            Color.blue
+        VStack {
+            Spacer()
+            Text("Guess the Flag")
+                .font(.largeTitle.weight(.bold))
+            VStack {
+                RadialGradient(stops: [
+                    .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                    .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+                ], center: .top, startRadius: 200, endRadius: 400)
                 .ignoresSafeArea()
-            
-            VStack (spacing: 30){
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
+                
+                VStack (spacing: 15){
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary).font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original).clipShape(Capsule()).shadow(radius: 5)
+                        }
+                    }
+                    .alert(scoreTitle, isPresented: $showingScore) {
+                        Button("Continue", action: askQuestion)
+                    } message: {
+                        Spacer()
+                        Spacer()
+                        Text("Your score is ???")
+                        Spacer()
+                    }
+                    
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .background(.regularMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
                 }
                 
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .renderingMode(.original)
-                    }
-                }
-                .alert(scoreTitle, isPresented: $showingScore) {
-                    Button("Continue", action: askQuestion)
-                } message: {
-                    Text("Your score is ???")
-                }
             }
+            Text("Score: ???")
+                .foregroundColor(.white)
+                .font(.title.bold())
+            Spacer()
         }
-        
+        .padding()
     }
     
     func askQuestion() {
