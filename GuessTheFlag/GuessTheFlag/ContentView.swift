2cedd2f85d994c0968d773d6163913dee6170b3d
Nedra Mevoli ♥ <nedra.mevoli@willowtreeapps.com>
2022-07-26 08:42:25 -0400

    Added most of the interface and assets, still need to bring in the score logic

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,17 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
-    @State private var currentScore = ""
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        Button("Show Alert"){
-            showingScore = true
-        }
-        .alert("Message", isPresented: $showingScore){
-            Button("OK"){
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack (spacing: 30){
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
                 
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+                .alert(scoreTitle, isPresented: $showingScore) {
+                    Button("Continue", action: askQuestion)
+                } message: {
+                    Text("Your score is ???")
+                }
             }
         }
+        
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
     }
 }
 
